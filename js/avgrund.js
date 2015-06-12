f26690294279b34960920d71e6ee8a3a4bceda68
Andrea da Fonseca <me@adf.ninja>
2015-06-12 02:12:37 +0200

    starting to work my way

--- js/avgrund.js
+++ js/avgrund.js
@@ -7,73 +7,90 @@
  */
 var Avgrund = (function(){
 
-	var container = document.documentElement,
-		popup = document.querySelector( '.avgrund-popup-animate' ),
-		cover = document.querySelector( '.avgrund-cover' ),
-		currentState = null;
-
-	container.className = container.className.replace( /\s+$/gi, '' ) + ' avgrund-ready';
+	var self = this,
+		popup = {},
+		wrapper = {},
+		container = {},
+		currentState = false
 
 	// Deactivate on ESC
 	function onDocumentKeyUp( event ) {
 		if( event.keyCode === 27 ) {
-			deactivate();
+			deactivate()
 		}
 	}
 
 	// Deactivate on click outside
 	function onDocumentClick( event ) {
-		if( event.target === cover ) {
-			deactivate();
+		console.log(event.target)
+		var bubble = event.target
+		while (bubble) {
+			if (bubble === popup)
+				return
+			else
+				bubble = bubble.parentNode
 		}
+		deactivate()
 	}
 
-	function activate( state ) {
-		document.addEventListener( 'keyup', onDocumentKeyUp, false );
-		document.addEventListener( 'click', onDocumentClick, false );
-		document.addEventListener( 'touchstart', onDocumentClick, false );
-
-		removeClass( popup, currentState );
-		addClass( popup, 'no-transition' );
-		addClass( popup, state );
+	function activate() {
+		addClass( popup, 'show-avgrund')
 
 		setTimeout( function() {
-			removeClass( popup, 'no-transition' );
-			addClass( container, 'avgrund-active' );
-		}, 0 );
+			addClass( container, 'avgrund-container-animate')
+				
+			document.addEventListener( 'keyup', onDocumentKeyUp, false )
+			container.addEventListener( 'click', onDocumentClick, false )
+			container.addEventListener( 'touchstart', onDocumentClick, false )
+		}, 0 )
 
-		currentState = state;
+		currentState = true
 	}
 
 	function deactivate() {
-		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
-		document.removeEventListener( 'click', onDocumentClick, false );
-		document.removeEventListener( 'touchstart', onDocumentClick, false );
+		document.removeEventListener( 'keyup', onDocumentKeyUp, false )
+		container.removeEventListener( 'click', onDocumentClick, false )
+		container.removeEventListener( 'touchstart', onDocumentClick, false )
 
-		removeClass( container, 'avgrund-active' );
-		removeClass( popup, 'avgrund-popup-animate')
+		removeClass( popup, 'show-avgrund')
+		removeClass( container, 'avgrund-container-animate')
+		
+		currentState = false
 	}
 
 	function disableBlur() {
-		addClass( document.documentElement, 'no-blur' );
+		addClass( document.documentElement, 'no-blur' )
 	}
 
 	function addClass( element, name ) {
-		element.className = element.className.replace( /\s+$/gi, '' ) + ' ' + name;
+		element.className += ' ' + name
 	}
 
 	function removeClass( element, name ) {
-		element.className = element.className.replace( name, '' );
+		element.className = element.className.replace( ' ' + name, '' )
 	}
 
 	function show(selector){
-		popup = document.querySelector( selector );
-		addClass(popup, 'avgrund-popup-animate');
-		activate();
-		return this;
+		popup = document.querySelector( selector )
+		container = document.getElementsByTagName('body')[0]
+		wrapper = document.createElement('div')
+		for (var i = -1, len = container.childNodes.length; ++i < len;) {
+			var child = container.childNodes[i]
+			if(child && child.tagName !== 'SCRIPT' && child.tagName !== 'STYLE' && child !== popup) {
+				wrapper.appendChild(child) && --i && --len
+			}
+		}
+		container.appendChild(wrapper)
+		container.appendChild(popup)
+		activate()
+		return self
 	}
 	function hide() {
-		deactivate();
+		for (var i = -1, len = wrapper.childNodes.length; ++i < len;) {
+			container.appendChild(wrapper.childNodes[i]) && --i && --len
+		}
+		container.removeChild(wrapper)
+		deactivate()
 	}
 
 	return {
@@ -84,4 +101,4 @@ var Avgrund = (function(){
 		hide: hide
 	}
 
-})();
+})()
